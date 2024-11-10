26303b859240965318506009d2f10469c0073ac7
cyrille <cyrilleotieno7@gmail.com>
2024-11-10 17:21:34 +0300

    fix get live matches

--- dist/addon.js
+++ dist/addon.js
@@ -12,22 +12,39 @@ Object.defineProperty(exports, "__esModule", { value: true });
 const stremio_addon_sdk_1 = require("stremio-addon-sdk");
 // Docs: https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/manifest.md
 const manifest = {
-    "id": "community.ppvstreams",
-    "version": "0.0.1",
-    "catalogs": [{ id: "basketball", type: "channel", name: "Live Basksetball matches" }, { id: "football", name: "Live Football matches", type: "channel" }],
-    "resources": [{ name: "stream", types: ["channel"] }, { name: "meta", types: ['channel'] }],
-    "types": ["channel"],
-    "name": "ppvstreams",
-    "description": "Watch live sports events and ppv streams from ppv land"
+    id: 'community.ppvstreams',
+    version: '0.0.1',
+    catalogs: [
+        { id: 'basketball', type: 'channel', name: 'Live Basksetball matches' },
+        { id: 'football', name: 'Live Football matches', type: 'channel' },
+        { id: 'Arm Wrestling', name: 'Live Arm Wrestling evens', type: 'channel' },
+        { id: 'Rugby', name: 'Live rugby matches', type: 'channel' },
+        { id: 'NFL', name: 'Live nfl matches', type: 'channel' },
+        {
+            id: 'Darts',
+            name: 'Live darts events around the world',
+            type: 'channel',
+        },
+    ],
+    resources: [
+        { name: 'stream', types: ['channel'] },
+        { name: 'meta', types: ['channel'] },
+    ],
+    types: ['channel'],
+    name: 'ppvstreams',
+    description: 'Watch live sports events and ppv streams from ppv land',
 };
 function getLiveFootballCatalog(id) {
     return __awaiter(this, void 0, void 0, function* () {
         var _a;
         try {
-            const matches = yield fetch("https://ppv.land/api/streams");
+            const matches = yield fetch('https://ppv.land/api/streams');
             const response = yield matches.json();
             const results = (_a = response.streams) !== null && _a !== void 0 ? _a : [];
-            const live = results.filter((a) => a.category.toLowerCase() == id).map((a) => a.streams).flat(2);
+            const live = results
+                .filter(a => a.category.toLowerCase() == id)
+                .map(a => a.streams)
+                .flat(2);
             return live;
         }
         catch (error) {
@@ -40,7 +57,14 @@ function getMovieStreams(id) {
         try {
             const streams = yield fetch(`https://ppv.land/api/streams/${id}`);
             const response = yield streams.json();
-            return [{ name: response.data.name, url: response.data.source, title: response.data.tag, behaviorHints: { notWebReady: true } }];
+            return [
+                {
+                    name: response.data.name,
+                    url: response.data.source,
+                    title: response.data.tag,
+                    behaviorHints: { notWebReady: true },
+                },
+            ];
         }
         catch (error) {
             return [];
@@ -52,18 +76,36 @@ function getMovieMetaDetals(id) {
         try {
             const streams = yield fetch(`https://ppv.land/api/streams/${id}`);
             const response = yield streams.json();
-            return { name: response.data.name, id: id, type: "channel", poster: response.data.poster, posterShape: "landscape", background: response.data.poster, language: "english", website: response.data.source, };
+            return {
+                name: response.data.name,
+                id: id,
+                type: 'channel',
+                poster: response.data.poster,
+                posterShape: 'landscape',
+                background: response.data.poster,
+                language: 'english',
+                website: response.data.source,
+            };
         }
         catch (error) {
-            return { id: id, name: "N/A", type: "channel", };
+            return { id: id, name: 'N/A', type: 'channel' };
         }
     });
 }
 const builder = new stremio_addon_sdk_1.addonBuilder(manifest);
 builder.defineCatalogHandler((_a) => __awaiter(void 0, [_a], void 0, function* ({ id }) {
-    const results = (yield getLiveFootballCatalog(id)).map((resp) => ({ id: resp.id.toString(), name: resp.name, type: "channel", background: resp.poster, description: resp.name, poster: resp.poster, posterShape: "landscape", logo: resp.poster }));
+    const results = (yield getLiveFootballCatalog(id)).map(resp => ({
+        id: resp.id.toString(),
+        name: resp.name,
+        type: 'channel',
+        background: resp.poster,
+        description: resp.name,
+        poster: resp.poster,
+        posterShape: 'landscape',
+        logo: resp.poster,
+    }));
     return {
-        metas: results
+        metas: results,
     };
 }));
 builder.defineMetaHandler((_a) => __awaiter(void 0, [_a], void 0, function* ({ id }) {
@@ -75,7 +117,7 @@ builder.defineMetaHandler((_a) => __awaiter(void 0, [_a], void 0, function* ({ i
 builder.defineStreamHandler((_a) => __awaiter(void 0, [_a], void 0, function* ({ id }) {
     const streams = yield getMovieStreams(id);
     return {
-        streams
+        streams,
     };
 }));
 exports.default = builder.getInterface();
