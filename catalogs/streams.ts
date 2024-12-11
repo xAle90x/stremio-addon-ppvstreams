5135aa2506d6807dcd66ec65be8a9e6e64208e69
cyrille <cyrilleotieno7@gmail.com>
2024-12-11 11:53:48 +0300

    fix timezone

--- catalogs/streams.ts
+++ catalogs/streams.ts
@@ -40,8 +40,8 @@ export const fetchWorldWideSportStreams = async (): Promise<DaddyliveStream[]> =
         } else {
             const request = await fetch('https://848b3516657c-worldwide-sports-tv.baby-beamup.club/catalog/tv/tv.json');
             const response = await request.json()
-            saveToCache('worldwide-events', JSON.stringify(response['metas']),24 * 60 * 60)
-            return (response['metas'] ?? [])?.filter((a:any)=>a.genres.includes('UK') || a.genres.includes('USA'))
+            saveToCache('worldwide-events', JSON.stringify(response['metas']), 24 * 60 * 60)
+            return (response['metas'] ?? [])?.filter((a: any) => a.genres.includes('UK') || a.genres.includes('USA'))
         }
     } catch (error) {
         Sentry.captureException(error)
@@ -60,13 +60,19 @@ export const fetchDaddyliveSchedule = async (): Promise<DaddyliveSchedule[]> => 
             const year = parts?.at(-1)
             const month = parts?.at(-2)
             const day = parts?.at(1)?.slice(0, 2)
-            const date = `${day} ${month} ${year}`
+            const date = dayjs(`${day} ${month} ${year}`)
+            const ukDateTime = `${year}-${date.format('MM')}-${day}T`;
+            const ukTimezone = "Europe/London";
+            // Convert to Kenya timezone (EAT is UTC+3)
+            const kenyaTimezone = "Africa/Nairobi";
+            
 
             for (const [showKey, showValue] of Object.entries(value)) {
                 const type = showKey.trim().replace(/ /gi, "-").toLowerCase()
                 for (let index = 0; index < showValue.length; index++) {
                     const event = showValue[index]
-                    events.push({ type, date: dayjs(`${date} ${event['time']}`).unix(), name: event.event, channels: Array.isArray(event.channels) ? event.channels?.map((a: any) => a.channel_name) : Object.values(event.channels)?.map((a: any) => a.channel_name) })
+                    const kenyaUnixTime = dayjs.tz(`${ukDateTime}${event['time']}:00`, ukTimezone).tz(kenyaTimezone).unix()
+                    events.push({ type, date: kenyaUnixTime, name: event.event, channels: Array.isArray(event.channels) ? event.channels?.map((a: any) => a.channel_name) : Object.values(event.channels)?.map((a: any) => a.channel_name) })
                 }
             }
         }
--- server.ts
+++ server.ts
@@ -1,11 +1,19 @@
 #!/usr/bin/env node
 import 'module-alias/register'
 import 'dotenv/config'
+import utc from 'dayjs/plugin/utc'
+import timezone from 'dayjs/plugin/timezone'
+
 import { serveHTTP } from "stremio-addon-sdk"
 import addonInterface from "./addon"
 import * as Sentry from "@sentry/node"
 import  { nodeProfilingIntegration } from "@sentry/profiling-node"
 import { buildCatalogCron } from "cronjobs"
+import dayjs from 'dayjs'
+
+dayjs.extend(utc)
+dayjs.extend(timezone)
+
 Sentry.init({
     dsn: "https://2faaad8d19ae0928c559d1ff0e81f093@o4504167984136192.ingest.us.sentry.io/4508274805374976",
     integrations: [ nodeProfilingIntegration()],
